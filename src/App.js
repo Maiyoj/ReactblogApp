c61ffa2fafbdf6a3fe16a71579bc8501f1b309a5
Maiyoj <valentinemaiyo1995@gmail.com>
2022-07-16 20:55:01 +0300

    add search component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 import Home from './components/Home';
 import { blog } from './components/data/blog';
+import Search from './components/Search';
 
 function App() {
 
   return (
     <div className="App">
+      <Search />
      <Home blogs={blog}/>
   
     </div>
---
+++ src/components/Search.js
@@ -0,0 +1,9 @@
+
+function Search(){
+    return (
+        <div className="Search">
+        <input placeholder="Search"></input>
+        </div>
+    )
+}
+export default Search;
